ee5c726d30f66907ae5ef9797049b4716759eca7
Fabian van Zetten <fnavanzetten@gmail.com>
2024-09-20 12:01:17 +0200

    Added sqllite database

--- Commands.cs
+++ Commands.cs
@@ -15,38 +15,90 @@ namespace DiscordNameChanger
         private readonly VotesDAO votesDAO = new();
 
         [SlashCommand("suggest", "Suggests a nickname for a user. Changes their name if it has the most votes")]
-        public async Task suggestNickname(IUser target, string nickname)
+        public async Task suggestNickname(IGuildUser target, string nickname)
         {
-
+            votesDAO.SetVote(Context.User.Id, target.Id, nickname);
+            await RespondAsync($"{Context.User.Mention} voted for {target.Mention} to be nicknamed {nickname}");
+            Dictionary<string, int> nicknames = votesDAO.GetAllSuggestions(target.Id);
+            string mostVotesNickname = nicknames.MaxBy(kvp => kvp.Value).Key;
+            if (!target.Username.Equals(mostVotesNickname))
+            {
+                await FollowupAsync($"Most voted username for {target.Mention} is now {nickname}");
+                await target.ModifyAsync(user => user.Nickname = mostVotesNickname);
+            }
         }
 
         [SlashCommand("show", "Description")]
         public async Task getNicknameSuggestions(IUser target)
         {
-
-            // EMBED
+            Dictionary<string, int> nicknames = votesDAO.GetAllSuggestions(target.Id);
+            string result = "";
+            if (nicknames.Count == 0)
+            {
+                result = $"Noone has voted for {target.Mention} yet. Use /nickname suggest and be the first!";
+            }
+            foreach (KeyValuePair<string, int> votes in nicknames)
+            {
+                result += $"{votes.Key}: {votes.Value}\n";
+            }
+            await RespondAsync(embed: new EmbedBuilder()
+            .WithAuthor("Nickname changer")
+            .WithTitle($"Nicknames for {target.GlobalName}")
+            .AddField("Votes", result).Build());
         }
 
-        [RequireRole(423101126171426816)]
+        //[RequireRole(423101126171426816)]
         public class AdminCommands : InteractionModuleBase<SocketInteractionContext>
         {
 
-            [SlashCommand("ban", "Bans the target user from suggesting nicknames")]
-            public async Task BanUserFromSuggestions(IUser target)
-            {
+            private readonly VotesDAO votesDAO = new();
 
+            private async Task ToggleBan(IGuildUser target, Boolean banned)
+            {
+                Dictionary<ulong, string> before = votesDAO.getAllResultsWhereUserVoted(target.Id), after;
+                votesDAO.setBannedStatus(target.Id, banned);
+                string text = banned ? "B" : "Unb";
+                await RespondAsync($"{text}anned {target.Mention} from suggesting nicknames");
+                after = votesDAO.getAllResultsWhereUserVoted(target.Id);
+                foreach (ulong targetId in before.Keys)
+                {
+                    if (!after.ContainsKey(targetId) || after[targetId] == null)
+                    {
+                        await FollowupAsync($"All votes for {target.Mention} have been removed. Changing name to {target.GlobalName}");
+                        await target.ModifyAsync(user => user.Nickname = target.GlobalName);
+                    }
+                    else if (!before[targetId].Equals(after[targetId]))
+                    {
+                        await FollowupAsync($"Most voted username for {target.Mention} is now {after[targetId]}");
+                        await target.ModifyAsync(user => user.Nickname = after[targetId]);
+                    }
+                }
             }
 
-            [SlashCommand("unban", "Unbans the target user from suggesting nicknames")]
-            public async Task UnbanUserFromSuggestions(IUser target)
+            [SlashCommand("ban", "Bans the target user from suggesting nicknames")]
+            public async Task BanUserFromSuggestions(IGuildUser target)
             {
+                await ToggleBan(target, true);
+            }
 
+            [SlashCommand("unban", "Unbans the target user from suggesting nicknames and invalidates all their votes")]
+            public async Task UnbanUserFromSuggestions(IGuildUser target)
+            {
+                await ToggleBan(target, false);
             }
 
             [SlashCommand("invalidate", "Invalidates the suggested nickname")]
             public async Task InvalidateNicknameSuggestion(String nickname)
             {
+                votesDAO.setInvalidateStatus(nickname, true);
+                await RespondAsync($"Invalidated nickname {nickname}");
+            }
 
+            [SlashCommand("uninvalidate", "Invalidates the suggested nickname")]
+            public async Task UninvalidateNicknameSuggestion(String nickname)
+            {
+                votesDAO.setInvalidateStatus(nickname, false);
+                await RespondAsync($"Validated nickname {nickname}");
             }
 
         }
--- VotesDAO.cs
+++ VotesDAO.cs
@@ -13,44 +13,57 @@ namespace DiscordNameChanger
             {
                 connection.Open();
 
+                //SqliteCommand command = connection.CreateCommand();
+                //command.CommandText = command.CommandText = @"DROP TABLE IF EXISTS votes";
+                //command.ExecuteNonQuery();
+                //command = connection.CreateCommand();
+                //command.CommandText = command.CommandText = @"DROP TABLE IF EXISTS voters";
+                //command.ExecuteNonQuery();
+                //command = connection.CreateCommand();
+                //command.CommandText = command.CommandText = @"DROP TABLE IF EXISTS nicknames";
+                //command.ExecuteNonQuery();
+
                 SqliteCommand command = connection.CreateCommand();
                 command.CommandText = @"CREATE TABLE IF NOT EXISTS voters (
-                    voter_id INTEGER PRIMARY KEY,
-                    banned BOOLEAN DEFAULT false";
+                    voter_id UNSIGNED BIG INT PRIMARY KEY,
+                    banned BOOLEAN DEFAULT false)";
                 command.ExecuteNonQuery();
 
                 command = connection.CreateCommand();
                 command.CommandText = command.CommandText = @"CREATE TABLE IF NOT EXISTS nicknames (
                     nickname_id INTEGER PRIMARY KEY,
-                    nickname TEXT NOT NULL,
-                    invalid BOOLEAN DEFAULT false";
+                    nickname TEXT NOT NULL UNIQUE,
+                    invalid BOOLEAN DEFAULT false)";
                 command.ExecuteNonQuery();
 
                 command = connection.CreateCommand();
                 command.CommandText = @"CREATE TABLE IF NOT EXISTS votes (
-                    voter_id INTEGER NOT NULL,
-                    nickname_id TEXT NOT NULL,
-                    target_id INTEGER NOT NULL,
+                    voter_id UNSIGNED BIG INT NOT NULL,
+                    nickname_id INTEGER NOT NULL,
+                    target_id UNSIGNED BIG INT NOT NULL,
                     PRIMARY KEY (voter_id, target_id),
                     FOREIGN KEY (nickname_id) REFERENCES nicknames (nickname_id),
-                    FOREIGN KEY (vote_id) REFERENCES voters (voter_id)";
+                    FOREIGN KEY (voter_id) REFERENCES voters (voter_id))";
                 command.ExecuteNonQuery();
             }
         }
 
-        public Dictionary<string, int> GetAllSuggestions(int userID)
+        public Dictionary<string, int> GetAllSuggestions(ulong userID)
         {
             Dictionary<string, int> result = [];
             using (var connection = new SqliteConnection(_source))
             {
                 connection.Open();
                 SqliteCommand command = connection.CreateCommand();
-                command.CommandText = @"SELECT nickname, SUM(*)
+                command.CommandText = @"SELECT nickname, COUNT(votes.voter_id) AS amount_of_votes
                                         FROM votes
                                         INNER JOIN nicknames on votes.nickname_id = nicknames.nickname_id
-                                        WHERE target_id = $target_id
+                                        INNER JOIN voters on votes.voter_id = voters.voter_id
+                                        WHERE target_id = $target_id 
+                                            AND NOT banned
+                                            AND NOT invalid
                                         GROUP BY nickname
-                                        ORDER BY SUM(*) ASC";
+                                        ORDER BY amount_of_votes ASC";
                 command.Parameters.AddWithValue("$target_id", userID);
                 using var reader = command.ExecuteReader();
                 while (reader.Read())
@@ -60,34 +73,7 @@ namespace DiscordNameChanger
             }
             return result;
         }
-
-        public void BanUser(int userID)
-        {
-            using (var connection = new SqliteConnection(_source))
-            {
-                connection.Open();
-                SqliteCommand command = connection.CreateCommand();
-                command.CommandText = @"insert or replace into voters (voter_id, banned) 
-                                        values ($voter_id, true)";
-                command.Parameters.AddWithValue("$voter_id", userID);
-                command.ExecuteNonQuery();
-            }
-        }
-
-        public void UnbanUser(int userID)
-        {
-            using (var connection = new SqliteConnection(_source))
-            {
-                connection.Open();
-                SqliteCommand command = connection.CreateCommand();
-                command.CommandText = @"insert or replace into voters (voter_id, banned) 
-                                        values ($voter_id, false)";
-                command.Parameters.AddWithValue("$voter_id", userID);
-                command.ExecuteNonQuery();
-            }
-        }
-
-        public void SetVote(int voter, int target, string nickname)
+        public void SetVote(ulong voter, ulong target, string nickname)
         {
             using (var connection = new SqliteConnection(_source))
             {
@@ -100,13 +86,77 @@ namespace DiscordNameChanger
                 command.ExecuteNonQuery();
 
                 command = connection.CreateCommand();
-                command.CommandText = @"insert or replace into votes (voter_id, target_id, nickname_id) 
-                                        values ($voter_id, $target_id, (SELECT nickname_id FROM nicknames WHERE nickname = $nickname)";
+                command.CommandText = @"insert or replace into voters (voter_id) 
+                                        values ($voter_id)";
                 command.Parameters.AddWithValue("$voter_id", voter);
-                command.Parameters.AddWithValue("$target_id", voter);
+                command.ExecuteNonQuery();
+
+                command = connection.CreateCommand();
+                command.CommandText = @"insert or replace into votes (voter_id, target_id, nickname_id) 
+                                        values ($voter_id, $target_id, (SELECT nickname_id FROM nicknames WHERE nickname = $nickname))";
+                command.Parameters.AddWithValue("$voter_id", voter);
+                command.Parameters.AddWithValue("$target_id", target);
                 command.Parameters.AddWithValue("$nickname", nickname);
                 command.ExecuteNonQuery();
             }
         }
+
+        public void setBannedStatus(ulong userID, Boolean banned)
+        {
+            using (var connection = new SqliteConnection(_source))
+            {
+                connection.Open();
+                SqliteCommand command = connection.CreateCommand();
+                command.CommandText = @"insert or replace into voters (voter_id, banned) 
+                                        values ($voter_id, $banned)";
+                command.Parameters.AddWithValue("$voter_id", userID);
+                command.Parameters.AddWithValue("$banned", banned);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void setInvalidateStatus(string nickname, Boolean invalidated)
+        {
+            using (var connection = new SqliteConnection(_source))
+            {
+                connection.Open();
+                SqliteCommand command = connection.CreateCommand();
+                command.CommandText = @"insert or replace into nicknames (nickname_id, nickname, invalid) 
+                                        values ((SELECT nickname_id from nicknames WHERE nickname = $nickname), $nickname, $invalidated)";
+                command.Parameters.AddWithValue("$nickname", nickname);
+                command.Parameters.AddWithValue("$invalidated", invalidated);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public Dictionary<ulong, string> getAllResultsWhereUserVoted(ulong voter)
+        {
+            Dictionary<ulong, string> result = [];
+            using (var connection = new SqliteConnection(_source))
+            {
+                connection.Open();
+                SqliteCommand command = connection.CreateCommand();
+                command.CommandText = @"WITH valid_votes AS (
+                                        SELECT target_id, nickname
+                                        FROM votes
+                                        INNER JOIN nicknames on votes.nickname_id = nicknames.nickname_id
+                                        INNER JOIN voters on votes.voter_id = voters.voter_id
+                                        WHERE NOT banned AND NOT invalid
+                                        GROUP BY target_id, nickname
+                                        ORDER BY COUNT(votes.voter_id) ASC
+                                        )
+                                        SELECT votes.target_id, (SELECT nickname FROM valid_votes WHERE valid_votes.target_id = votes.target_id LIMIT 1) 
+                                        FROM votes
+                                        WHERE voter_id = $voter";
+                command.Parameters.AddWithValue("$voter", voter);
+                using var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    result.Add((ulong)reader.GetInt64(0), reader.GetString(1));
+                }
+            }
+            return result;
+        }
+
     }
 }
